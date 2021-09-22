2868314faecaa9f3839b03e79e7bdc2c93e622e5
vluck <val@itsacloudlife.com>
2021-09-21 20:05:34 -0700

    paramaratize the values

--- main.tf
+++ main.tf
@@ -1,22 +1,16 @@
 module "wafv2" {
   source        = "trussworks/wafv2/aws"
   version       = "2.4.0"
-  name          = "dev-cloudfront-web-acl"
-  scope         = "CLOUDFRONT"
-  associate_alb = false #associated below
+
+  name          = var.name
+  scope         = var.scope
+  associate_alb = var.associate_alb
   managed_rules = [
     {
       name            = "AWSManagedRulesCommonRuleSet",
       priority        = 10
       override_action = "none"
-      excluded_rules  = [ "EC2MetaDataSSRF_BODY",
-                          "NoUserAgent_HEADER",
-                          "GenericRFI_BODY",
-                          "GenericRFI_URIPATH",
-                          "CrossSiteScripting_COOKIE",
-                          "CrossSiteScripting_BODY",
-                          "SizeRestrictions_BODY",
-                          "SizeRestrictions_QUERYSTRING" ]
+      excluded_rules  = []
     },
     {
       name            = "AWSManagedRulesAmazonIpReputationList",
@@ -52,20 +46,19 @@ module "wafv2" {
 }
 
 
-output "dev_cdn_wafv2" {
+output "web_acl_id" {
    value = module.dev_cdn_wafv2.web_acl_id
 }
 
-resource "aws_ssm_parameter" "waf_cl_id_dev" {
-  provider    = aws.uswest2
-  name        = "/waf_acl_id/dev"
-  description = "the waf_acl_id for dev"
+resource "aws_ssm_parameter" "waf_web_acl_id" {
+  name        = "/${var.namespace}/${var.stage}/waf/${var.name}"
+  description = "the waf_acl_id for ${var.name}"
   type        = "String"
   value       = module.dev_cdn_wafv2.web_acl_id
   overwrite   = true
 
   tags = {
-    environment = "dev"
+    environment = var.stage
     notes       = "managed by terraform"
   }
 }
